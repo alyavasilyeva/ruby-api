f1877f27645e1acfd56c9e97315878e5fc6f6779
alyavasilyeva <delirium.of.insane@gmail.com>
2019-11-28 12:15:43 +0300

    generate access token

---
+++ spec/factories/access_tokens.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :access_token do
+    association :user
+  end
+end
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -1,47 +1,3 @@
-# require 'rails_helper'
-
-# describe UserAuthenticator do
-#   describe '#perform' do
-#     let(:authenticator) { described_class.new('sample_code') }
-
-#     subject { authenticator.perform }
-
-#     context 'when code is incorrect' do
-#       # let(:error) {
-#       #   double("Sawyer::Resource", error: "bad_verification_code")
-#       # }
-#       # before do
-#       #   allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return(error)
-#       # end
-
-#       let(:error) {
-#         double("Sawyer::Resource", error: "bad_verification_code")
-#       }
-
-#       before do
-#         allow_any_instance_of(Octokit::Client).to receive(
-#           :exchange_code_for_token).and_return(error)
-#       end
-
-#       it 'should raise an error' do
-#         # expect{ subject }.to raise_error(UserAuthenticator::AuthenticationError)
-#         # expect(authenticator.user).to be_nil
-#         expect{ subject }.to raise_error(
-#           UserAuthenticator::AuthenticationError
-#         )
-#         expect(authenticator.user).to be_nil
-
-#       end
-#     end
-
-#     context 'when code is correct' do
-#       it 'should save the user when does not exist' do
-#         expect { subject }.to change{ User.count }.by(1)
-#       end
-#     end
-#   end
-# end
-
 require 'rails_helper'
 
 describe UserAuthenticator do
@@ -89,6 +45,17 @@ describe UserAuthenticator do
       it 'should save the user when does not exists' do
         expect{ subject }.to change{ User.count }.by(1)
         expect(User.last.name).to eq('John Smith')
+      end
+
+      it 'shoud reuse already registered user' do
+        user = create :user, user_data
+        expect{ subject }.not_to change{ User.count }
+        expect(authenticator.user).to eq(user)
+      end
+
+      it "should create and set user's access token" do
+        expect{ subject }.to change{ AccessToken.count }.by(1)
+        expect(authenticator.access_token).to be_present
       end
     end
   end
